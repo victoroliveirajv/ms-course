84069ba4d0ea366af4616f9e8e9598d9e9c941b0
Victor Oliveira <victoroliveirajv10@gmail.com>
2024-09-19 20:49:04 -0300

    RestTamplate

--- hr-payroll/src/main/java/com/victorjv/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/victorjv/hr_payroll/services/PaymentService.java
@@ -2,12 +2,31 @@ package com.victorjv.hr_payroll.services;
 
 
 import com.victorjv.hr_payroll.entities.Payment;
+import com.victorjv.hr_payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Pedro Augusto", 200.0, days);
+
+        Map<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("id", Long.toString(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariable);
+        return new Payment(worker.getName(),worker.getDailyIncome(), days);
     }
 }
