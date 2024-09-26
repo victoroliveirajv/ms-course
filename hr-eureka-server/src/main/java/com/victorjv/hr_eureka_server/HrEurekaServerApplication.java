e4b03294a078da1b86ed1b0edba1166bb8e764fc
Victor Oliveira <victoroliveirajv10@gmail.com>
2024-09-26 20:53:49 -0300

    Created hr-eureka-server

---
+++ hr-eureka-server/src/main/java/com/victorjv/hr_eureka_server/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.victorjv.hr_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
