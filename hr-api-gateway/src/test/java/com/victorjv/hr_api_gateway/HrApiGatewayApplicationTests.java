c9e8bf530023903cfd248486d2b3393c112efe96
Victor Oliveira <victoroliveirajv10@gmail.com>
2024-10-14 19:59:43 -0300

    Project hr-api-gateway created

---
+++ hr-api-gateway/src/test/java/com/victorjv/hr_api_gateway/HrApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorjv.hr_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
