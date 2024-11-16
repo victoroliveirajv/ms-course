b2c9141961da19ef3dcc9b5d22cbd56e20f00b5f
Victor Oliveira <victoroliveirajv10@gmail.com>
2024-11-16 18:38:05 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/com/victorjv/hr_config_server/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/victorjv/hr_config_server/HrConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.victorjv.hr_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
