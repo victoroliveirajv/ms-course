cd4141b469e67ab1f2d3c8060e5da9a04f140980
Victor Oliveira <victoroliveirajv10@gmail.com>
2024-11-16 20:15:25 -0300

    hr-woker as a config server client

--- hr-worker/src/main/java/com/victorjv/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/victorjv/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import com.victorjv.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import java.util.List;
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Value("${test.config}")
+    private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -49,5 +53,12 @@ public class WorkerResource {
         Worker worker = repository.findById(id).get();
 
         return ResponseEntity.ok(worker);
+    }
+
+    @GetMapping(value ="/configs")
+    public ResponseEntity<Void> getConfigs(){
+        logger.info("CONFIG = " + testConfig);
+
+        return ResponseEntity.noContent().build();
     }
 }
