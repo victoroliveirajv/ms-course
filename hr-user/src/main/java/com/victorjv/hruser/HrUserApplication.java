ce0353e4a2e6e3b15d71db30a204d572ae446502
Victor Oliveira <victoroliveirajv10@gmail.com>
2024-11-24 13:25:36 -0300

    Configuring hr-user project

--- hr-user/src/main/java/com/victorjv/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/victorjv/hruser/HrUserApplication.java
@@ -1,4 +1,4 @@
-package com.victoroliveirajv.hruser;
+package com.victorjv.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
