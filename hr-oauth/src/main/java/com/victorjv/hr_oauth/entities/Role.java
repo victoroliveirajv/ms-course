83f26b1b6825a3b80312a55881fc332c513c2dc1
Victor Oliveira <victoroliveirajv10@gmail.com>
2024-11-25 21:08:10 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/victorjv/hr_oauth/entities/Role.java
@@ -0,0 +1,46 @@
+package com.victorjv.hr_oauth.entities;
+
+import java.util.Objects;
+
+
+public class Role {
+
+    private Long id;
+    private String roleName;
+
+    public Role(){
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role role)) return false;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
